c22b941f2df58cf64ec44b9e44c6e04317b0fd36
Thanh Luan <thanhluando25@gmail.com>
2024-09-26 10:55:37 +0700

    first commit

---
+++ assets/js/slider.js
@@ -0,0 +1,33 @@
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img");
+
+const imgContainer = document.querySelector(".aspect-ratio-169");
+
+const dotItems = document.querySelectorAll(".dot");
+
+let imgNumber = imgPosition.length;
+let index = 0;
+imgPosition.forEach(function (image, index) {
+    image.style.left = index * 100 + "%";
+    dotItems[index].addEventListener("click", function (e) {
+        slider(index);
+    });
+});
+
+function imgSlide() {
+    index++;
+
+    if (index >= imgNumber) {
+        index = 0;
+    }
+
+    slider(index);
+}
+
+function slider(index) {
+    imgContainer.style.left = "-" + index * 100 + "%";
+    const dotActive = document.querySelector(".active");
+    dotActive.classList.remove("active");
+    dotItems[index].classList.add("active");
+}
+
+setInterval(imgSlide, 5000);
